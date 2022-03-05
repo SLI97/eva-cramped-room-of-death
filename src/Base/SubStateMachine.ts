e307e88027b36e925c46ee0e0b075919676a0086
liangchaogui <liangchaogui@bytedance.com>
2022-03-05 21:29:06 +0800

    private补充

--- src/Base/SubStateMachine.ts
+++ src/Base/SubStateMachine.ts
@@ -6,7 +6,7 @@ import StateMachine from './StateMachine';
  * 用处：例如有个idle的state，但是有多个方向，为了让主状态机更整洁，可以把同类型的但具体不同的state都封装在子状态机中
  */
 export default abstract class SubStateMachine {
-  _currentState: State = null;
+  private _currentState: State = null;
   stateMachines: Map<string, State> = new Map();
 
   constructor(public fsm: StateMachine) {}
